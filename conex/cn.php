d18d73c6ab061592b9ea6187c3191a10700d58df
Pestus0 <juancruzgutierrez2005@gmail.com>
2024-12-10 16:38:21 -0300

    arreglo

--- conex/cn.php
+++ conex/cn.php
@@ -1,11 +1,2 @@
 <?php
 $conexdb = mysqli_connect('localhost', 'root', '', 'linkedhouses');
-
-// Verificar la conexión
-if (!$conexdb) {
-    die("Conexión fallida: " . mysqli_connect_error());
-} else {
-    echo "Conexión exitosa.";
-}
-?>
-
