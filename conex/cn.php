653157287becd32b882f404091a734b9c656885c
juan <gohanelssj1@gmail.com>
2024-12-10 16:39:47 -0300

    prueba de carrito

--- conex/cn.php
+++ conex/cn.php
@@ -1,11 +1,4 @@
 <?php
 $conexdb = mysqli_connect('localhost', 'root', '', 'linkedhouses', 3307);
-
-// Verificar la conexión
-if (!$conexdb) {
-    die("Conexión fallida: " . mysqli_connect_error());
-} else {
-    echo "Conexión exitosa.";
-}
 ?>
 
