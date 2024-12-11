2d2695b7fcf4884525b93e810f800bebec871736
Pestus0 <juancruzgutierrez2005@gmail.com>
2024-12-11 14:54:13 -0300

    boton para cerrar la publicacion

--- JS/vermas.js
+++ JS/vermas.js
@@ -6,12 +6,16 @@ function showDetails(details) {
     modalBody.innerHTML = `
         <h2>${details.Tipo} en ${details.Localidad}</h2>
         <img src="${details.Imagen}" alt="Imagen" style="max-width: 100%; height: auto;">
-        <p><strong>DNI Dueño:</strong> ${details['Dni-dueño']}</p>
         <p><strong>Ambientes:</strong> ${details['Cant-ambientes']}</p>
         <p><strong>Fecha:</strong> ${details.Fecha}</p>
         <p><strong>Método de Pago:</strong> ${details['Met-pago']}</p>
         <p><strong>Condiciones:</strong> ${details.Condiciones}</p>
+        <button id="close-modal-button" style="margin-top: 10px; padding: 5px 10px; background-color: red; color: white; border: none; border-radius: 5px; cursor: pointer;">Cerrar</button>
     `;
+
+    const closeButton = modalBody.querySelector('#close-modal-button');
+    closeButton.addEventListener('click', closeDetails);
+
     modal.style.display = 'block';
 }
 
@@ -20,4 +24,3 @@ function closeDetails() {
     const modal = document.getElementById('details-modal');
     modal.style.display = 'none';
 }
-
